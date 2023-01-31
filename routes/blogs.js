895de4138e136a816fb6ad6e828b1be8b4e4fbec
MeshaS32 <kamesha.hamilton@codeimmersives.com>
2023-01-31 14:38:18 -0500

    initial commit

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users page');
+  res.send('blogs page');
 });
 
 module.exports = router;
